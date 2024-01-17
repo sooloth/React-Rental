07fdd15c2d3379c7b635ebeab88e4f99e0b37a84
sooloth <pangnuar@gmail.com>
2024-01-17 08:45:41 +0700

    first

---
+++ src/Content.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { useState } from "react"; //react hook
+import { FaTrashAlt } from "react-icons/fa"; //icons
+
+const Content = () => {
+  const [items, setItem] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "one half pound bag of Cocoa Covered Almonds Unsalted",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "item 3",
+    },
+  ]);
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItem(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItem(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+  return (
+    <main>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+              />
+              <label
+                style={item.checked ? { textDecoration: "line-through" } : null}
+                onDoubleClick={() => handleCheck(item.id)}
+              >
+                {item.item}
+              </label>
+              <FaTrashAlt
+                onClick={() => handleDelete(item.id)}
+                role="button"
+                tabIndex="0"
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your list is empty.</p>
+      )}
+    </main>
+  );
+};
+
+export default Content;
